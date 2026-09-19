30a0f1cabca1b6b39786cec6b91854cfeb9e5fdb
Saurabh-pandey-495 <dev.Saurabh495@gmail.com>
2026-09-20 00:54:46 +0530

    msf

---
+++ lang/en/contact.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+
+    'meta_title' => 'Contact Us',
+
+    'meta_description' => 'Get in touch with Team 0001. We would love to hear from you.',
+
+    'badge' => 'Get In Touch',
+
+    'hero_title' => 'Let’s Start a Conversation.',
+
+    'hero_description' => 'Have a question, idea, collaboration proposal, or simply want to connect with Team 0001? Send us a message.',
+
+    'info_badge' => 'Contact Team 0001',
+
+    'info_title' => 'We’re Here to Listen.',
+
+    'info_description' => 'Whether you want to know more about Team 0001, participate in an activity, collaborate with us, or share an idea, feel free to reach out.',
+
+    'email_label' => 'Email',
+
+    'phone_label' => 'Phone',
+
+    'address_label' => 'Address',
+
+    'follow_us' => 'Follow Us',
+
+    'form_badge' => 'Send a Message',
+
+    'form_title' => 'Tell Us What’s On Your Mind.',
+
+    'form_description' => 'Fill out the form below and our team will get back to you as soon as possible.',
+
+    'validation_title' => 'Please fix the following:',
+
+    'name' => 'Full Name',
+
+    'name_placeholder' => 'Enter your full name',
+
+    'email' => 'Email Address',
+
+    'email_placeholder' => 'you@example.com',
+
+    'phone' => 'Phone Number',
+
+    'phone_placeholder' => '+91 XXXXX XXXXX',
+
+    'subject' => 'Subject',
+
+    'subject_placeholder' => 'Select a subject',
+
+    'subjects' => [
+
+        'general' => 'General Inquiry',
+
+        'membership' => 'Team Membership',
+
+        'collaboration' => 'Collaboration',
+
+        'events' => 'Events & Activities',
+
+        'feedback' => 'Feedback',
+
+        'support' => 'Technical Support',
+
+        'other' => 'Other',
+    ],
+
+    'message' => 'Message',
+
+    'message_placeholder' => 'Write your message here...',
+
+    'form_note' => 'Please provide accurate information so our team can respond appropriately.',
+
+    'submit' => 'Send Message',
+    'success_message' => 'Thank you for contacting Team 0001. We have received your message and will get back to you soon.',
+
+    'sending' => 'Sending...',
+];
