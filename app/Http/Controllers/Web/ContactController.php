8e5e664c8c5a2f9e752e29306732ca71c65ec226
Saurabh-pandey-495 <dev.Saurabh495@gmail.com>
2026-09-21 14:10:34 +0530

    message

--- app/Http/Controllers/Web/ContactController.php
+++ app/Http/Controllers/Web/ContactController.php
@@ -82,6 +82,7 @@ class ContactController extends Controller
                 'string',
                 'max:10',
             ],
+            // data nhi ja rha 
 
             'subject' => [
                 'required',
