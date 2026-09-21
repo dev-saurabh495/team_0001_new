4184389c1418b98f72e1cce8698a92d81f36ccb7
Saurabh-pandey-495 <dev.Saurabh495@gmail.com>
2026-09-21 14:07:47 +0530

    messages

--- resources/views/Web/pages/contact.blade.php
+++ resources/views/Web/pages/contact.blade.php
@@ -29,7 +29,7 @@
 
 
                         <span>
-                            Contact
+                            {{ __('contact.breadcrumb') }}
                         </span>
                     </nav>
 
